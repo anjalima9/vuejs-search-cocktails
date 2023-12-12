e633c319544569100a0948626470e4f875aecf81
anjalima9 <andrianatoandro.anja@gmail.com>
2023-12-12 18:17:08 +0300

    fix the style

--- src/components/CocktailItem.vue
+++ src/components/CocktailItem.vue
@@ -1,19 +1,20 @@
 <template>
-    <div class="bg-white shadow rounded-xl">
+    <div class="bg-white shadow rounded-xl relative hover:scale-105 transition-all">
             <router-link :to="{name: 'cocktailDetails', params: {id: cockt.idDrink}}">
                 <img :src="cockt.strDrinkThumb" :alt="cockt.strDrink" class="rounded-t-xl w-full h-60 object-cover">
             </router-link>
             <div class="p-3">
                 <h3 class="font-bold" >{{ cockt.strDrink }}</h3>
-                <p class="mb-4">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                In fringilla orci non eros molestie, volutpat tempor tellus tempor.
-                 </p>
-            <div>
+                <p class="mb-10">
+                    {{ cockt.strInstructions.slice(0, 100) }}{{ cockt.strInstructions.length > 100 ? '...' : '' }}
+    </p>
+
+            <div class="absolute bottom-0 left-3 mb-3 ml-3">
                 <YoutubeButton :href="cockt.strVideo">Youtube</YoutubeButton>
             </div>
             </div>
         </div>
+
 </template>
 
 <script setup>
